84a4ed5e5edf63927924c60964681720990c3c82
Abdellatif Chabrik <achabrik@irevolution.com>
2023-11-10 19:01:47 +0100

    initial commit

---
+++ src/main/java/com/example/childscrappermicroservice/ChildScrapperMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.childscrappermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildScrapperMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildScrapperMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/childscrappermicroservice/ChildScrapperMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.childscrappermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildScrapperMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
